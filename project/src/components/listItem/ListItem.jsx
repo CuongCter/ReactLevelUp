c057a59f323c9aeee9683022b17d246d9b6df8d4
CuongCter <cuongcter442002@gmail.com>
2022-08-10 23:06:35 +0700

    update

--- project/src/components/listItem/ListItem.jsx
+++ project/src/components/listItem/ListItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './ListItem.scss'
-const ListItem = ({ id, item, handleDelete, handleChange, completed }) => {
+const ListItem = ({ id, item, handleDelete, handleChange, completed, handleEdit, handleActive, handleSave }) => {
     return (
         <div className='item mt-10 '>
             <div className='item_todo'>
@@ -8,11 +8,30 @@ const ListItem = ({ id, item, handleDelete, handleChange, completed }) => {
                     <div className='item_todo__icon' onClick={() => handleChange(id)}>
                         <i class="fa-solid fa-check"></i>
                     </div>
-                    <div className='item_todo__plan'>{item.plan}</div>
+                    {
+                        !item.change ? (<div className='item_todo__plan'>{item.plan}</div>)
+                            : (<input
+                                type='text'
+                                value={item.plan}
+                                onChange={(e) => handleActive(e, id)}
+                                onKeyUp={(e) => handleSave(e, id)}
+                            />)
+                    }
+
                 </div>
 
-                <div className='item_todo__delete' onClick={() => handleDelete(id)}>
-                    <i class="fa-solid fa-circle-minus"></i>
+                <div className='item_todo__change'>
+                    <div onClick={() => handleEdit(id)}>
+                        {
+                            !item.change ? (
+                                <i class="fa-solid fa-pen mr-3"></i>
+                            ) : (<i class="fa-solid fa-circle-check mr-3"></i>)
+                        }
+
+                    </div>
+                    <div onClick={() => handleDelete(id)}>
+                        <i class="fa-solid fa-circle-minus"></i>
+                    </div>
                 </div>
             </div>
         </div >
--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -3,49 +3,81 @@ import { useState } from 'react'
 import AddItem from '../components/addItem/AddItem'
 import ListItem from '../components/listItem/ListItem'
 import './Home.scss'
-const arrTodo = [
-    {
-        plan: "Study",
-        completed: false,
-    },
-    {
-        plan: "Play football",
-        completed: false,
-    }
-]
+
 const Home = () => {
     const [todo, setTodo] = useState({
         plan: "",
         completed: false,
+        change: false
     })
-    const [listTodo, setListTodo] = useState([])
+    const local = () => {
 
+    }
+    const [listTodo, setListTodo] = useState([])
+    //Add
     const handleAdd = (e) => {
         if (e.keyCode === 13) {
-            const arr = [...listTodo];
-            arr.push(todo);
-            setListTodo(arr);
-            setTodo({
-                plan: "",
-                completed: false,
-            })
+            if (todo.plan === "") {
+                alert("You have not entered the plan")
+            }
+            else {
+                const arr = [...listTodo];
+                arr.push(todo);
+                setListTodo(arr);
+                setTodo({
+                    plan: "",
+                    completed: false,
+                })
+            }
+
         }
     }
+    //Delete
     const handleDelete = (idx) => {
         const arrUpdate = [...listTodo];
         arrUpdate.splice(idx, 1);
         setListTodo(arrUpdate);
     }
+    //Complete
     const handleChange = (id) => {
         const arrUpdate = [...listTodo];
         arrUpdate.length > 0 && arrUpdate.map((item, idx) => {
-          if (idx === id) {
-            return item.completed =  !item.completed
-          }
-          return item
+            if (idx === id) {
+                return item.completed = !item.completed
+            }
+            return item
         })
         setListTodo(arrUpdate)
-      }
+    }
+    //Edit
+    const handleEdit = (id) => {
+        const arr = [...listTodo]
+        arr && arr.map((item, index) => {
+            if (index === id) return (item.change = !item.change)
+            return item
+        })
+        setListTodo(arr)
+    }
+    const handleActive = (e, id) => {
+        const arr = [...listTodo]
+        arr && arr.map((item, index) => {
+            if (index === id) {
+                return (item.plan = e.target.value)
+            }
+            return item
+        })
+
+        setListTodo(arr);
+    }
+    const handleSave = (e, id) => {
+        const arr = [...listTodo]
+        arr && arr.map((item, index) => {
+            if (index === id && e.keyCode === 13)
+                return item.change = !item.change
+            return item
+        })
+
+    }
     return (
         <div className='main'>
             <AddItem todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
@@ -55,9 +87,14 @@ const Home = () => {
                         <ListItem
                             id={index}
                             item={item}
-                            handleDelete  = {handleDelete}
-                            handleChange = {handleChange}
-                            completed={item.completed}/>
+                            handleDelete={handleDelete}
+                            handleChange={handleChange}
+                            completed={item.completed}
+                            handleEdit={handleEdit}
+                            handleActive={handleActive}
+                            handleSave={handleSave}
+                        />
+
                     </li>
                 ))}
             </ul>
